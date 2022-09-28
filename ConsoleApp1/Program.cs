29fdf18e9b3c5a24aefff349d9e8d5533d3e721e
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-09-28 12:03:59 +0300

    Created two methods expandedForm and expandedForm2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,49 +10,19 @@ namespace ConsoleApp1
             //expandedForm(42); // Should return '40 + 2'
             //expandedForm(70304); // Should return '70000 + 300 + 4'
 
+            Console.WriteLine("12: " + expandedForm(12));
+            Console.WriteLine("42: " + expandedForm(42));
+            Console.WriteLine("70304: " + expandedForm(70304));
 
-            //Console.WriteLine(expandedForm(12));
-            //expandedForm2(153);
-            Console.WriteLine(expandedForm2(70304));
-            //double d = 702;
-            //var m = d / 10;
-            // m = d % 10;
-            //m = m * 10;
+            Console.WriteLine("\n" + new string('-', 30) + "\n");
 
-            //Console.WriteLine(m);
+            Console.WriteLine("12: " + expandedForm2(12));
+            Console.WriteLine("42: " + expandedForm2(42));
+            Console.WriteLine("70304: " + expandedForm2(70304));
         }
-
 
 
         static string expandedForm(int value)
-        {
-            string result = default;
-            double val = default;
-            string length = value.ToString();
-
-
-                for (int i = 0; i < length.Length; i++)
-                {
-                    val = value;
-                    val = val % 10;
-                    val = val * Math.Pow(10, i);
-                    
-                    value = value / 10;
-                    if (i != length.Length - 1)
-                    {
-                        result += val + " + ";
-                    }
-                    else
-                    {
-                        result += val;
-                    }
-                }
-           
-
-            return result;
-        }
-
-        static string expandedForm2(int value)
         {
             string result = default;
             double val = default;
@@ -90,16 +60,49 @@ namespace ConsoleApp1
             return result;
         }
 
-        //static string expandedForm(int value)
-        //{
-        //    string result = null;
-        //    string val = value.ToString();
+        static string expandedForm2(int value)
+        {
+            string result = default;
+            double val = default;
+            string val2 = default;
+            string length = value.ToString();
+            string[] arr = new string[length.Length];
 
-        //    for (int i = 0; i <= val.Length; i++)
-        //    {
-        //        result += val.Substring(i, 1) + new string('0', val.Length - i-1) + "+";
-        //    }
-        //    return result;
-        //}
+
+            for (int i = 0; i < length.Length; i++)
+            {
+                val = value;
+                val = val % 10;
+                val2 = val.ToString() +  "*" + Math.Pow(10, i).ToString();
+
+                value = value / 10;
+
+                if (val != 0)
+                {
+                    arr[i] = val2;
+                }
+               
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (arr[i] == null)
+                {
+                    continue;
+                }
+                else if (i != 0)
+                {
+                    result += arr[i] + " + ";
+                }
+                else
+                {
+                    result += arr[i];
+                }
+            }
+
+            return result;
+        }
+
+
     }
 }
